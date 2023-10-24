d9b4661dc41c613e9c29e1fdcd21c18c623c6342
abdulrxz <abdulrehman.xcs@gmail.com>
2023-10-24 14:57:20 +0500

    Changes commit

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PRODUCTS } from "../../products";
+import { PRODUCTS } from "../../product";
 import { Product } from "./product";
 import "./shop.css";
 
@@ -7,7 +7,7 @@ export const Shop = () => {
   return (
     <div className="shop">
       <div className="shopTitle">
-        <h1>PedroTech Shop</h1>
+        <h1>E Shop</h1>
       </div>
 
       <div className="products">
